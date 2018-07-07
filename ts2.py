74ac3a6f30c2804cf7bb14c0e4f11416a7abfdcd
Helmut <hheisego@gmail.com>
2018-07-06 20:41:28 -0500

    Update ts2.py

--- ts2.py
+++ ts2.py
@@ -3,25 +3,22 @@
 import crassh
 import datetime
 import time
-#import sleep
 import sys
 
 # Variables
-##routers = ["10.232.120.1", "10.233.8.2", "10.232.24.13", "10.232.24.14", "10.232.24.15"]
+## devices list:
+routers = ["10.232.120.1", "10.233.8.2", "10.232.24.13", "10.232.24.14", "10.232.24.15"]
 
-routers = ["10.232.120.1", "10.233.8.2", "10.232.24.13", "10.232.24.14", "10.232.24.15", "10.232.248.2", "10.232.248.3", "10.232.248.11",
-		   "10.232.248.12", "10.232.248.13", "10.232.248.14", "10.232.248.15", "10.232.248.16", "10.232.248.17", "10.232.248.18", "10.232.248.19"] 
-
-username = "heisegoh"
-password = "Welcome123"
+username = "hheisego"
+password = "savehours123"
 
 var1 = len(routers)
 var2 = var1
-#Date and Time
 
+#Date and Time
 time = datetime.datetime.now()
 
-filename = raw_input("Introduce the Report file name :  ")  # Python 2
+filename = raw_input("Introduce the Report file name :  ")  
 print filename
 
 #wipe the file to have it clean files :)
@@ -37,7 +34,7 @@ for device in routers:
 			
         	crassh.disconnect()
 
-# Split the output by spaces so we can search the response
+# Split the output by spaces so we can search the response -- to be released in v2.0
         	words = output.split()
 			
 		
@@ -45,19 +42,14 @@ for device in routers:
 		
 		print hostname, "Information gathered, writting to file --> ", filename, " remaining: ", var2, "of: ", var1
 		
-# progress bar
-		#progress = progressbar.ProgressBar()
-		#for i in progress(range(80)):
-		#	time.sleep(0.01)
+
 		
-		
-		
-#ciclo de escritora del pinche archivo
-#	for i in range(2):
+#to write output in the report file
+
 		f.write("\r\n\r\n ------>>Hostname: %s<<-----report time: %s-------------------- \r\n\r\n %s " % (hostname, time, output))
 
 
-	#closing fucking fil
+	#closing file
 		f.close()
 
 	except:
